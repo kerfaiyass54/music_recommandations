0e69641b55ac9cbee80e2a09fbfbf473256e17eb
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2025-12-12 15:50:38 +0100

    Added wordcloud stats and visualization

--- main.py
+++ main.py
@@ -13,15 +13,31 @@ from nltk.tokenize import word_tokenize
 df = pd.read_csv("./spotify_millsongdata.csv")
 
 
-print("Number of data in this CSV: " + df.shape)
+print("\nNumber of data in this CSV: " + df.shape)
 
-print("First 5 data rows: " + df.head())
+print("\nFirst 5 data rows: " + df.head())
 
-print("Information about the dataset: /n" + df.info())
+print("\nInformation about the dataset: " + df.info())
 
-print("Number of null entries: " + df.isnull.sum())
+print("\nNumber of null entries: " + df.isnull.sum())
+
+top_artists = df['artist'].value_counts().head(10)
+print("\nTop 10 artists: " + top_artists)
 
 
+df = df.sample(10000)
+df = df.drop('link', axis=1).reset_index(drop=True)
+
+
+all_lyrics = " ".join(df['text'].dropna())
+wordcloud = WordCloud(width=800, height=400, background_color="white").generate(all_lyrics)
+
+
+plt.figure(figsize=(10, 5))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.title("Most Common Words in Lyrics")
+plt.show()
 
 
 
